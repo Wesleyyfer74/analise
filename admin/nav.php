838f111e693cef57b134865de8f19cd66a5ceca1
Wesleyyfer74 <Wesleyyfer74@users.noreply.github.com>
2026-07-07 10:15:48 -0400

    Point admin site link to public landing

--- admin/nav.php
+++ admin/nav.php
@@ -3,6 +3,6 @@
     <a class="<?= $current_admin_page === 'index.php' ? 'active' : '' ?>" href="index.php">Dashboard</a>
     <a class="<?= $current_admin_page === 'clientes.php' ? 'active' : '' ?>" href="clientes.php">Clientes</a>
     <a class="<?= $current_admin_page === 'financeiro.php' ? 'active' : '' ?>" href="financeiro.php">Financeiro</a>
-    <a href="../index.php">Site</a>
+    <a href="https://gerador.spacered.com.br/">Site</a>
     <a href="logout.php">Sair</a>
 </nav>
